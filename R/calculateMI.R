55996fc0e223fb39d24a29aab93937de12ee0315
saraharcos <sarah.e.arcos@gmail.com>
2022-11-16 12:51:01 -0500

    adding tests and starting to simplify calculateMI functions

--- R/calculateMI.R
+++ R/calculateMI.R
@@ -4,27 +4,46 @@ library(data.table)
 library(foreach)
 library(doSNOW)
 
-run_mi <- function(phy, mat){
-  print("calculating mi")
-  mi <- calculate_mi(mat)
-  print("calculating meaned mi")
-  mi_m <- calculate_meaned_mi(mat, phy)
-  return(list("mi" = mi, "mi_m" = mi_m))
-}
-
-calculate_mi <- function(mat){
-  entropies <- vector(length = ncol(mat))
-  for(i in 1:ncol(mat)){
-    entropies[i] = get_ent_unweighted(i, mat)
+#Main function
+calculateMI <- function(msa_matrix,
+                        weighted = TRUE,
+                        groups = NULL,
+                        weights = "equal",
+                        ncores = 2){
+  if(weighted){
+    mi <- calculate_weighted_mi(msa_matrix, groups, weights, ncores)
+  }
+  else{
+    mi <- calculate_raw_mi(msa_matrix, ncores)
   }
   
+  return(mi)
+}
+
+get_entropies <- function(mat, weighted = TRUE, groups = NULL, weights = NULL){
+  entropies <- vector(length = ncol(mat))
+  if(weighted){
+    for(i in 1:ncol(mat)){
+      entropies[i] = get_ent_meaned(i, groups, mat)
+    }
+  }
+  else{
+    for(i in 1:ncol(mat)){
+      entropies[i] = get_ent_unweighted(i, mat)
+    }
+  }
+  return(entropies)
+}
+
+#Calculate MI without any weighting
+calculate_raw_mi <- function(mat, ncores){
+  entropies <- get_entropies(mat, weighted = FALSE)
   non_zero_e <- which(entropies!=0)
   
-  joint_cols <- t(combn(non_zero_e, 2)) %>%
-    as.data.frame()
+  joint_cols <- as.data.frame(t(combn(non_zero_e, 2)))
   
-  cl <- makeCluster(4, type="SOCK") # for 4 cores machine
-  clusterExport(cl, c("get_jointent_unweighted", "calculate_mi"))
+  cl <- makeCluster(ncores, type="SOCK") # for 4 cores machine
+  clusterExport(cl, c("get_jointent_unweighted", "calculate_raw_mi"))
   registerDoSNOW (cl)
   
   pb <- txtProgressBar(max = nrow(joint_cols), style = 3)
@@ -43,78 +62,34 @@ calculate_mi <- function(mat){
   return(format_mi(entropies, joint_ents, joint_cols))
 }
 
-format_mi <- function(entropies, joint_entropies, joint_cols){
-  joint_cols$joint_entropy <- joint_entropies
-  non_zero_e <- which(entropies!=0)
-  
-  v1_entropy <- numeric(nrow(joint_cols))
-  v2_entropy <- numeric(nrow(joint_cols))
-  for (i in 1:nrow(joint_cols)) {
-    v1_entropy[i] <- entropies[joint_cols$V1[i]]
-    v2_entropy[i] <- entropies[joint_cols$V2[i]]
-  }
-  
-  joint_cols$v1_entropy <- v1_entropy
-  joint_cols$v2_entropy <- v2_entropy
-  joint_cols$mi <- (v1_entropy + v2_entropy) - joint_entropy
-  
-  meanMI <- mean(joint_cols$mi)
-  
-  mean_MIs <- numeric(length(non_zero_e))
-  for (i in non_zero_e){
-    mean_MIs[i] <- joint_cols[joint_cols$V1 == i | joint_cols$V2 == i,] %>% pull(mi) %>% mean()
-  }
-  
-  v1_meanMI <- numeric(nrow(joint_cols))
-  v2_meanMI <- numeric(nrow(joint_cols))
-  for (i in 1:nrow(joint_cols)) {
-    v1_meanMI[i] <- mean_MIs[joint_cols$V1[i]]
-    v2_meanMI[i] <- mean_MIs[joint_cols$V2[i]]
-  }
-  
-  joint_cols$v1_meanMI <- v1_meanMI
-  joint_cols$v2_meanMI <- v2_meanMI
-  joint_cols$apc <- (v1_meanMI*v2_meanMI)/meanMI
-  joint_cols$mip <- joint_cols$mi - joint_cols$apc
-  joint_cols$Group <- paste("[", joint_cols$V1, ";", joint_cols$V2, "]", sep = "")
-  
-  return(joint_cols)
-}
-
+#Get Shannon entropy
 get_ent_unweighted <- function(residue, mat){
-  t <- table(mat[,residue]) %>%
-    prop.table()
+  t <- prop.table(table(mat[,residue]))
   
   return(sum(-t*log2(t)))
 }
 
+#Get joint entropy
 get_jointent_unweighted <- function(residue_a,residue_b, mat){
   jointcol <- as.matrix(paste(mat[,residue_a],mat[,residue_b],sep=""))
   
-  t <- table(jointcol) %>%
-    prop.table()
+  t <- prop.table(table(jointcol))
   
   return(sum(-t*log2(t)))
 }
 
-calculate_meaned_mi <- function(mat, df){
-  ents <- c()
-  for(i in 1:ncol(mat)){
-    ents[i] = get_ent_meaned(i, df, mat)
-    if(i%%100 == 0){
-      print(paste(i, "out of", ncol(mat), sep  = " "))
-    }
-  }
+#Caculate MI with weights (equal or defined)
+calculate_weighted_mi <- function(mat, groups, weights, ncores){
+  ents <- get_entropies(mat, groups = groups)
   
   non_zero_e <- which(ents!=0)
   
   print("doing combn step")
-  joint_cols <- t(combn(non_zero_e, 2)) %>%
-    as.data.frame()
+  joint_cols <- as.data.frame(t(combn(non_zero_e, 2)))
   
   print("starting clusters")
-  cl <- makeCluster(6, type="SOCK") # for 4 cores machine
-  clusterExport(cl, c("get_jointent_meaned", "calculate_meaned_mi"))
+  cl <- makeCluster(ncores, type="SOCK") # for 4 cores machine
+  clusterExport(cl, c("get_jointent_meaned", "calculate_weighted_mi"))
   registerDoSNOW (cl)
   
   pb <- txtProgressBar(max = nrow(joint_cols), style = 3)
@@ -124,7 +99,7 @@ calculate_meaned_mi <- function(mat, df){
   joint_ents <- foreach(i = 1:nrow(joint_cols), .combine="c", .packages=c('data.table'),
                         .options.snow = opts) %dopar%
     {
-      get_jointent_meaned(joint_cols[i, "V1"], joint_cols[i, "V2"], df, mat)
+      get_jointent_meaned(joint_cols[i, "V1"], joint_cols[i, "V2"], groups, mat)
     }
   
   close(pb)
@@ -134,6 +109,7 @@ calculate_meaned_mi <- function(mat, df){
   return(format_mi(ents, joint_ents, joint_cols))
 }
 
+#Get equal-weighted Shannon entropy
 get_ent_meaned <- function(pos, df, mat){
   groups <- unique(df$date)
   
@@ -156,6 +132,7 @@ get_ent_meaned <- function(pos, df, mat){
   return(sum(-log2(t1frw)*t1frw))
 }
 
+#Get equal-weighted joint entropy
 get_jointent_meaned <- function(a, b, df, mat){
   groups <- unique(df$date)
 
@@ -176,4 +153,43 @@ get_jointent_meaned <- function(a, b, df, mat){
   t12frw <- colMeans(t12frbind, na.rm = TRUE)
   
   return(sum(-log2(t12frw)*t12frw))
+}
+
+#Format MI results, regardless of weighting
+format_mi <- function(entropies, joint_entropies, joint_cols){
+  joint_cols$joint_entropy <- joint_entropies
+  non_zero_e <- which(entropies!=0)
+  
+  v1_entropy <- numeric(nrow(joint_cols))
+  v2_entropy <- numeric(nrow(joint_cols))
+  for (i in 1:nrow(joint_cols)) {
+    v1_entropy[i] <- entropies[joint_cols$V1[i]]
+    v2_entropy[i] <- entropies[joint_cols$V2[i]]
+  }
+  
+  joint_cols$v1_entropy <- v1_entropy
+  joint_cols$v2_entropy <- v2_entropy
+  joint_cols$mi <- (v1_entropy + v2_entropy) - joint_entropies
+  
+  meanMI <- mean(joint_cols$mi)
+  
+  mean_MIs <- numeric(length(non_zero_e))
+  for (i in non_zero_e){
+    mean_MIs[i] <- mean(pull(joint_cols[joint_cols$V1 == i | joint_cols$V2 == i,], mi))
+  }
+  
+  v1_meanMI <- numeric(nrow(joint_cols))
+  v2_meanMI <- numeric(nrow(joint_cols))
+  for (i in 1:nrow(joint_cols)) {
+    v1_meanMI[i] <- mean_MIs[joint_cols$V1[i]]
+    v2_meanMI[i] <- mean_MIs[joint_cols$V2[i]]
+  }
+  
+  joint_cols$v1_meanMI <- v1_meanMI
+  joint_cols$v2_meanMI <- v2_meanMI
+  joint_cols$apc <- (v1_meanMI*v2_meanMI)/meanMI
+  joint_cols$mip <- joint_cols$mi - joint_cols$apc
+  joint_cols$Group <- paste("[", joint_cols$V1, ";", joint_cols$V2, "]", sep = "")
+  
+  return(joint_cols)
 }
---
+++ R/tests.R
@@ -0,0 +1,40 @@
+#Tests for calculateMI functions
+
+
+library(tidyverse)
+library(purrr)
+library(data.table)
+library(foreach)
+library(doSNOW)
+library(readxl)
+library(lubridate)
+library(Biostrings)
+
+seq_times <- readRDS("../timeMI/data/seq_times_091122.rda")
+seq_times_rndate <- seq_times %>%
+  select(rowname, date)
+
+h3n2_pol <- readAAStringSet("/Users/saraharcos/Desktop/Lauring Lab/H3N2_Processing/pol_aa.fasta") %>%
+  as.matrix()
+
+h3n2_pol_small <- h3n2_pol[,1:100]
+
+h3n2_meaned_mi_noHA <- calculateMI(h3n2_pol_small, groups = seq_times_rndate)
+h3n2_mi_noHA <- calculateMI(h3n2_pol_small, weighted = FALSE)
+
+#Compare
+mi_weighted_res <- readRDS("../timeMI/data/MI_results_091122/h3n2_meaned_mi_noHA.rds") %>%
+  filter(V1 <= 100 &
+           V2 <= 100)
+
+sum(h3n2_meaned_mi_noHA$mi == mi_weighted_res$mi)
+
+
+
+
+
+
+
+
+
+
